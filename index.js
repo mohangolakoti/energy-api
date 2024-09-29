fe5757104d6b9d139b58a106606e3a4e9fd2fe9f
Mohan <ramgolakoti1@gmail.com>
2024-09-29 15:12:17 +0530

    port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", sensorDataRoutes);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.MONGO_URL)
